3a1861e2ceff4c970e74db2bf251434e09be87b7
Azat Khuzhin <a3at.mail@gmail.com>
2014-03-30 18:03:43 +0400

    dcinfo: simplest walk by filesystems list impl

--- dcinfo.c
+++ dcinfo.c
@@ -42,6 +42,34 @@ static void __exit dcInfoExit(void)
 
 static int dcInfoShow(struct seq_file *m, void *v)
 {
+    char buf[PAGE_SIZE]; /** XXX dynamically */
+    int len = get_filesystem_list(buf);
+
+    if (len <= 0) {
+        return 0;
+    }
+
+    while (len > 0) {
+        char dev[PATH_MAX], name[PATH_MAX]; /** XXX dynamically */
+        struct file_system_type **p, *fs;
+        struct dentry *dentry;
+
+        len -= sscanf(buf, "%s %s\n", dev, name);
+        seq_printf(m, "Filesystem: %s\n", name);
+        p = find_filesystem(name, strlen(name));
+        fs = *p;
+        if (fs) {
+            continue;
+        }
+
+        rcu_read_lock();
+        list_for_each_entry_rcu(dentry, &fs->s_dentry_lru, d_lru) {
+            seq_printf(m, "%pd4\n", dentry);
+        }
+        fs->s_dentry_lru;
+        rcu_read_unlock();
+    }
+
     return 0;
 }
 
